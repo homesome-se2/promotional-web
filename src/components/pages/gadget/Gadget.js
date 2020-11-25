eb35d904ef6141b53b19f63be1a09e5cc54322cb
Ali Muhammed <Alawi93@live.se>
2020-11-25 20:14:21 +0100

    :art: Added pages
    Added functional components that will be used for each page, pages include the hoso teams, main page and about page etc

---
+++ src/components/pages/gadget/Gadget.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Gadget() {
+    return (
+        <>
+            
+        </>
+    )
+}
+
+export default Gadget
---
+++ src/components/pages/publicServer/PublicServer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PublicServer() {
+    return (
+        <>
+            
+        </>
+    )
+}
+
+export default PublicServer
