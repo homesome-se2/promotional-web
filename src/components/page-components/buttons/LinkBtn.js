d38e8f0afbbe20216fd646782231a63fd250a16b
Ali Muhammed <Alawi93@live.se>
2020-11-28 13:39:54 +0100

    :sparkles: Added page components
    Added page sections that will be used in all the pages. Added cards that will be used for the feature section and added buttons that will be used to access github and clients (android and web).

---
+++ src/components/page-components/buttons/LinkBtn.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './LinkBtn.css';
+
+function LinkBtn() {
+    return (
+        <>
+      <a target="_blank" rel="noopener noreferrer" href={props.src}>
+      <div className="linkBtn" >
+      <div className="icon">{props.icon}</div>
+      <ul>
+          <li><a>{props.top}</a></li>
+          <li><a>{props.bottom}</a></li>
+      </ul>
+      
+    </div>
+    </a>     
+        </>
+    )
+}
+
+export default LinkBtn
