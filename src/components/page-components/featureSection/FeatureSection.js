e46f72c58b154eb67759f6101f3c4526420d4b8e
Ali Muhammed <Alawi93@live.se>
2020-11-30 09:58:21 +0100

    :sparkles: Added a  feature section
    The cards will display teams features in this section. Added new dependency, react elastic card.

--- src/components/page-components/featureSection/FeatureSection.js
+++ src/components/page-components/featureSection/FeatureSection.js
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from 'react';
+import './FeatureSection.css';
+import Card from '../../page-components/cards/FeatureCard';
+import Carousel from "react-elastic-carousel";
+import {InfoSectionContainer, TopLine , Text } from './FeatureSectionElement';
 
-function FeatureSection() {
+function FeatureSection({
+    items,pattern
+}) {
+    const [mainText,setMainText] = useState('');
+
+    function highligted(text,id){
+
+    setMainText(text)
+
+    var element = document.getElementById(id);
+      element.classList.add("highlight");
+     
+    setTimeout(() => {
+        element.classList.remove("highlight")
+      }, 1500);
+    }
+    const number = 2000;
+    const displayItems = [
+        {width: 500, itemsToShow: 1},
+        {width: 768, itemsToShow: 1},
+        {width: 900, itemsToShow: 1},
+        {width: 1400, itemsToShow: 1},
+        {width: 1600, itemsToShow: 1},
+    ]
+
     return (
         <>
-            
+          <InfoSectionContainer>  
+            <div >
+            <TopLine>Features</TopLine>
+
+                <Carousel  breakPoints={displayItems} className="carousel">
+                {items.map((item,i)=>(
+                <div onClick={(e)=> highligted(item.text,i)} className="item">
+
+                    <Card key={i} id={i}
+                    img={item.image}
+                    heading={item.heading}
+                    description={item.intro}
+                    />
+                  
+                  </div> 
+                    ))}
+                    </Carousel>
+                <Text>{mainText}</Text>  
+            </div>
+            </InfoSectionContainer>  
         </>
     )
 }
--- src/components/page-components/featureSection/FeatureSectionElement.js
+++ src/components/page-components/featureSection/FeatureSectionElement.js
@@ -1,1 +1,68 @@
 import styled from 'styled-components';
+
+export const InfoSectionContainer = styled.div`
+display: flex;
+margin: auto;
+margin-top: 5%;
+justify-content: center;
+height: 1050px;
+z-index: 1;
+
+`
+
+
+export const TopLine = styled.h6`
+-webkit-text-stroke: 0.7px black;
+position: static; 
+margin-left: auto;
+margin-right: auto;
+left: 0;
+right: 0;
+text-align: center;
+text-transform: uppercase;
+letter-spacing: 6px;
+font-size: 35px;
+font-weight: 800;
+color: #1a83ff;
+
+`
+
+export const Text = styled.p`
+background: #f5f0f0;
+opacity: 0.7;
+margin-top: 20px;
+text-align: left;
+font-size: 25px;
+width: 1000px;
+overflow: hidden;
+word-break: normal;
+
+@media screen and (max-width: 1400px){
+    width: 730px;
+  }
+
+@media screen and (max-width: 1000px){
+    width: 730px;
+  }
+
+@media screen and (max-width: 968px){
+    margin-top: 20px;
+    font-size: 20px;
+    width: 570px;
+}
+
+@media screen and (max-width: 801px){
+  width: 590px;
+}
+
+@media screen and (max-width: 768px){
+    margin-top: 20px;
+    font-size:18px;
+    width: 580px;
+}
+
+@media screen and (max-width: 601px){
+    font-size: 13px;  
+    width: 380px;
+}
+`
