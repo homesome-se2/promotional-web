b0cc3c3de4555aa30e9ba9dfdd0a8a15b468f5e2
Ali Muhammed <Alawi93@live.se>
2020-12-01 22:04:54 +0100

    :sparkles: Added gallerySection
    Added not implemented yet. Images from the different part of the system will be displayed.

--- src/components/page-components/gallerySection/GallerySectionElement.js
+++ src/components/page-components/gallerySection/GallerySectionElement.js
@@ -1,2 +1,28 @@
 import styled from 'styled-components';
 
+export const PreviewContainer = styled.div`
+width:100%;
+height:100%;
+margin: 0;
+padding: 0;
+box-sizing: border-box;
+overflow: hidden;
+
+`
+export const Gallerys = styled.div`
+padding: 40px;
+border-color: white;
+border-style: solid;
+display: column;
+flex-wrap: wrap;
+height: auto;
+
+`
+
+export const Inner = styled.div`
+width: 100%;
+max-width: 1200px;
+margin: auto;
+display: flex;
+
+`
